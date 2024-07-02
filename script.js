b073d5948a5b2b0f522c71f5e8df283c8ccbb901
thiSzAbhi <iamyap@proton.me>
2024-07-02 22:50:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function playVideo() {
     const link = document.getElementById('videoLink').value;
     const videoPlayer = document.getElementById('videoPlayer');
-    
+
     let embedLink;
     if (link.includes('mdisk.me')) {
-        embedLink = `https://mdisk.me/embed/${extractId(link)}`;
-    } else if (link.includes('terabox')) {
-        embedLink = `https://terabox.com/embed/${extractId(link)}`;
-    } else if (link.includes('pdisk')) {
-        embedLink = `https://pdisk.me/embed/${extractId(link)}`;
+        embedLink = `https://mdisk.me/embed-${extractId(link)}`;
+    } else if (link.includes('terabox.com')) {
+        embedLink = `https://www.terabox.com/embed/${extractId(link)}`;
+    } else if (link.includes('pdisk.me')) {
+        embedLink = `https://pdisk.me/embed-${extractId(link)}`;
     } else {
         alert('Unsupported link');
         return;
     }
-    
-    videoPlayer.innerHTML = `<iframe src="${embedLink}" width="640" height="360" frameborder="0" allowfullscreen></iframe>`;
+
+    videoPlayer.innerHTML = `<iframe src="${embedLink}" allowfullscreen></iframe>`;
 }
 
 function downloadVideo() {
@@ -28,6 +28,6 @@ function downloadVideo() {
 }
 
 function extractId(link) {
-    const parts = link.split('/');
-    return parts[parts.length - 1];
+    const url = new URL(link);
+    return url.pathname.split('/').pop();
 }
